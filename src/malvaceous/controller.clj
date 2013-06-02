4fccd698c995b63f134d38be565a4197273f76a9
ujihisa <ujihisa@gmail.com>
2013-06-02 01:32:28 -0700

    add controller

---
+++ src/malvaceous/controller.clj
@@ -0,0 +1,17 @@
+(ns malvaceous.controller)
+
+(defn- http-request [routing method path params]
+  (let [p (promise)]
+    (if-let [handler (routing [method path])]
+      (future
+        (handler p params))
+      (deliver p [404 nil]))
+    p))
+
+(def METHOD->method
+  {"GET" 'get "POST" 'post "PUT" 'put "DELETE" 'delete})
+
+(defn make [routing]
+  (fn [method path]
+    (let [p (http-request routing (METHOD->method method) path {})]
+      (deref p 2000 [500 "timeout"]))))
--- src/malvaceous/example.clj
+++ src/malvaceous/example.clj
@@ -1,4 +1,5 @@
 (ns malvaceous.example
+  (:require [malvaceous.controller :as c])
   (:require [malvaceous.webserver :as w]))
 
 (def routing
@@ -10,26 +11,5 @@
                  (deliver p
                    [200 (format "welcome, %s!" (get params :name "somebody"))]))})
 
-(defn http-request [method path params]
-  (let [p (promise)]
-    (if-let [handler (routing [method path])]
-      (future
-        (handler p params))
-      (deliver p [404 nil]))
-    p))
-
-(def METHOD->method
-  {"GET" 'get "POST" 'post "PUT" 'put "DELETE" 'delete})
-
 (defn -main []
-  (w/run 8080 (fn [method path]
-                (prn [:method method :path path])
-                (let [p (http-request (METHOD->method method) path {})]
-                  (deref p 2000 [500 "timeout"]))))
-  #_(let [p (http-request 'get "/" {})]
-    (let [[code body] (deref p 2000 [500 :timeout])]
-      (prn :code code :body body)))
-  #_(let [p (http-request 'post "/" {:name "uj"})]
-    (let [[code body] (deref p 2000 [500 :timeout])]
-      (prn :code code :body body)))
-  #_(shutdown-agents))
+  (w/run 8080 (c/make routing)))
